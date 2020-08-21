c150f1ad625ca20275dafec51c7c38d56a528559
ROME84 <22250311+Rome84@users.noreply.github.com>
2020-08-20 22:06:42 -0400

    Update KUBE-DOCKER.sh

--- scripts/KUBE-DOCKER.sh
+++ scripts/KUBE-DOCKER.sh
@@ -10,22 +10,37 @@
 #################################################################################################                                                                                           
                                                                                           
 echo "Install linux development tools"
-sudo yum -y groupinstall "Development Tools"
+yum -y groupinstall "Development Tools"
 
 echo "Updating your OS to latest OS..."
-sudo yum update -y
+yum update -y
 
 echo "Install the dependencies"
-sudo yum install wget git curl tree htop vim net-tools nc -y
+yum install epel-release wget git curl tree htop vim net-tools nc  -y
+
+echo "Creating docker group"
+groupadd docker
+
+echo "Creating docker user"
+useradd -d /opt/docker -m -g docker docker
+
+echo "Adding docker to the wheel group"
+usermod -aG wheel docker
+
+echo "Giving Splunk Ownership"
+chown -R docker:docker /opt/docker/
 
 echo "Download the docker package from docker.com..."
-sudo wget https://download.docker.com/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
+wget https://download.docker.com/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
+
+echo "Go to docker binary path"
+cd /opt/docker/bin
 
 echo "Install the docker package"
 sudo yum install -y --setopt=obsoletes=0  docker-ce-17.03.1.ce-1.el7.centos docker-ce-selinux 17.03.1.ce-1.el7.centos
 
 echo "Add your Linux user to docker group to execute docker commands without sudo"
-sudo usermod -aG docker vagrant
+sudo usermod -aG docker docker
 
 echo "Start the Docker service, check the status and add docker service to boot"
 sudo systemctl start docker
@@ -62,34 +77,49 @@ exclude=kube*
 
 EOF
 
-## install kubelet, kubeadm and kubectl
+## installing kubelet, kubeadm and kubectl
 sudo yum install -y --disableexcludes=kubernetes kubelet kubeadm kubectl
 
-## enable and start kubelet
+## enabling and start kubelet
 sudo systemctl enable kubelet
 sudo systemctl start kubelet
 
-## enable bash completion for both
+## enabling bash completion for both
 sudo kubeadm completion bash > /etc/bash_completion.d/kubeadm
 sudo kubectl completion bash > /etc/bash_completion.d/kubectl
 
 ## activate the completion
-sudo . /etc/profile
+. /etc/profile
 
-# copy the credentials to your user
-sudo mkdir -p $HOME/.kube
+# copying the credentials to your user
+mkdir -p $HOME/.kube
 cat /etc/kubernetes/admin.conf > $HOME/.kube/config
-sudo chmod 600 $HOME/.kube/config
+chmod 600 $HOME/.kube/config
 
-# install networking
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
+# installing networking
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
 
-# let master node be used as regular node (put pods there) (optional)
-sudo kubectl taint nodes --all node-role.kubernetes.io/master-
+# letting master node be used as regular node (put pods there) (optional)
+kubectl taint nodes --all node-role.kubernetes.io/master-
 
-# Append the FQN to the /etc/hosts
-sudo echo "192.168.33.28 capacitybay28.example.com capacity28" >>/etc/hosts
-sudo echo "capacitybay01.example.com" >/etc/hostname
+# Appending  the FQDN to the /etc/hosts
+echo "192.168.33.28 capacitybay28.example.com capacity28" >>/etc/hosts
+echo "capacitybay01.example.com" >/etc/hostname
+
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=30000-32767/tcp                                                   
+firewall-cmd --permanent --add-port=179/tcp
+firewall-cmd --permanent --add-port=4789/udp
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --permanent --add-port=8000/tcp
+firewall-cmd --permanent --add-port=6443/tcp
+firewall-cmd --permanent --add-port=2379-2380/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10251/tcp
+firewall-cmd --permanent --add-port=10252/tcp
+firewall-cmd --permanent --add-port=179/tcp
+firewall-cmd --permanent --add-port=4789/udp
 
 # Reboot the server
 sudo init 6
